f71302b7be74ad8f10975467f01a62b28b5d3178
KidiroInfiniti <39047042+KidiroInfiniti@users.noreply.github.com>
2020-02-27 00:44:08 -0600

    start clock

--- welcome.js
+++ welcome.js
@@ -1,12 +1,16 @@
-var canvas = document.getElementById('clock');
-var ctx = canvas.getContext('2d');
-var radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-drawClock();
+window.addEventListener("load", function(event){
+  var canvas = document.getElementById('clock');
+  var ctx = canvas.getContext('2d');
+  canvas.width = 400;
+  canvas.height = 400
+  var radius = canvas.height / 2;
+  ctx.translate(radius, radius);
+  radius = radius * 0.90
+  drawClock();
 
-function drawClock() {
-  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
-  ctx.fillStyle = "white";
-  ctx.fill();
-}
+  function drawClock() {
+    ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+});
